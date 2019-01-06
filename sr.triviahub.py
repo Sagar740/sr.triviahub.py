66a6b391abf3f8fc7c62a9fd1d504ece1bbda95a
Jeeturay <45664400+Jeeturay@users.noreply.github.com>
2019-01-06 13:08:14 +0530

    Update sr.triviahub.py

--- sr.triviahub.py
+++ sr.triviahub.py
@@ -838,8 +838,8 @@ async def on_member_remove(member):
   msg = "Goodbye {}".format(member.mention)
   await client.send_message(channel, msg) 
 
-
 client.run(os.getenv('Token'))
+client.run(os.getenv('Token')
 
 
 
